497991d8db095f9a12f20719fe9a6daf1e7b229e
samcotton1 <sam.cotton1234@gmail.com>
2019-05-22 00:57:55 +0100

    Update Graph.java
    Add JavaDoc Comments to the code

--- Graph.java
+++ Graph.java
@@ -9,9 +9,25 @@ import javafx.scene.*;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Graph {
+/**
+ * Graph.java Class
+ * @author Samuel Cotton
+ *This Class deals with all of the front end parts of the code i.e:
+ *User Interaction
+ *Displaying the Game Arena and objects in it 
+ * 
+ */
 
-	public void Graph4() {
+public class Graph {
+/**
+ * 
+ * GUI function
+ * Sets up the Nodes for the user in random places that can be moved
+ * If statement will then determine what the user does
+ * 
+ */
+	public void GUI() {
+		
 	 GameArena window4 = new GameArena(500,500);	
 		
 		Ball[] ballArray = new Ball[7];
@@ -30,12 +46,27 @@ public class Graph {
 			
 			Random rand = new Random();
 
+			/**
+			 * @param ball1X. Stores random number from 0 to 450
+			 * @param ball1Y. Stores random number from 0 to 450
+			 * 
+			 * @param ball2X. Stores random number from 0 to 450
+			 * @param ball2X. Stores random number from 0 to 450
+			 * 
+			 */
+			
 			int ball1X = rand.nextInt(450);
 			int ball1Y = rand.nextInt(450);
 						
 			int ball2X = rand.nextInt(450);
 			int ball2Y = rand.nextInt(450);
 	
+			/**
+			 * Creates object ballArray with the variables used above
+			 * The i is used to add more than one to the Array, using
+			 * The for loop to add 1 each time to i
+			 */
+			
 			ballArray[i]  = new Ball(ball1X, ball1Y, 20, "BLUE");
 					
 			window4.addBall(ballArray[i]);
@@ -43,6 +74,12 @@ public class Graph {
 		}
 		window4.update();
 		int Y = 1;
+		
+		/**
+		 * If statement
+		 * Determines what the user does
+		 * Loops until the exit else if is called e.g. 5
+		 */
 		
 		while (Y == 1) {
 			System.out.print("What Would you like to do?\n 1 = Move Node  \n 2 = Add Text \n 3 = Add Arc\n 4 = Add Arrows \n 5 = exit\n");
